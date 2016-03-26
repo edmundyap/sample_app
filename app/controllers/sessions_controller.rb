470e8dc5ee1227d45e90f64d6d388d00b93bbd7a
Next <nextacademy@email.com>
2016-03-26 22:08:25 +0800

    Add account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,19 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-    	log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-    	redirect_back_or user
+        if user.activated?
+    	   log_in user
+         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+    	   redirect_back_or user
+        else
+         #flash.now because render is not counted as a request. Without it, the flash will persist until the next request.
+         message = "Account not activated."
+         message += "Check your email for the activation link."
+         flash[:warning] = message
+         redirect_to root_url
+        end
     else
-      #flash.now because render is not counted as a request. Without it, the flash will persist until the next request.
-    	flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination'
   		render 'new'
   	end
   end
