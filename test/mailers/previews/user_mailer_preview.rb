0848991e8dd74c5f592ee67661e675c2ccaa2c2f
Next <nextacademy@email.com>
2016-03-30 01:27:38 +0800

    latest changes

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,13 +2,13 @@
 class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
-  def account_activation
-    UserMailer.account_activation
+  def account_activation(user)
+    @user = user
+    mail to: user.email, subject: "Account activation"
   end
-
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
-  def password_reset
-    UserMailer.password_reset
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"
   end
-
 end
