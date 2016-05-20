f2893fac4c3871e773e3742dc54978846a709fcf
Next <nextacademy@email.com>
2016-05-20 17:05:17 +0800

    deleted comment view files

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,9 +3,6 @@ class CommentsController < ApplicationController
 before_action :logged_in_user, only: [:create, :destroy]
 before_action :correct_user, only: :destroy
 
-  def new
-  end
-
 	def create
 		@comment = current_user.comments.build(comment_params)
     @comment.micropost_id = params[:micropost_id]
